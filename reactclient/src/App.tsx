96f97a2f1daff033a4433816ac119d109985819b
Roberto Ciano <roberto@second-page.de>
2019-05-11 19:34:51 +0200

    Add node/sass and material-ui/icons and clean up a bit

--- reactclient/src/App.tsx
+++ reactclient/src/App.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import { AppBar, Link, Paper, Toolbar } from '@material-ui/core';
+import GroupIcon from '@material-ui/icons/Group';
 import { BrowserRouter as Router, Link as RouteLink, Route } from 'react-router-dom';
 import { AuthProvider } from './Auth/AuthContext';
-import { AuthService } from './AuthService';
+import { AuthService } from './Auth/AuthService';
+import { TeamList } from './Team/TeamList';
+import { TeamInterface } from './interfaces/Team.interface';
 
 function Index() {
   return <h2>Home</h2>;
-}
-
-function About() {
-	return <h2>About</h2>;
 }
 
 function Users() {
@@ -18,8 +17,10 @@ function Users() {
 
 const MyLink = (props: { href?: string }): React.ReactElement => <RouteLink to={props.href || 'foo'} {...props}/>
 
-class AppRouterBase extends React.PureComponent<undefined, { isLoggedIn: boolean }> {
+export class App extends React.PureComponent<{}, { isLoggedIn: boolean }> {
 	private readonly authService = new AuthService();
+
+	state = { isLoggedIn: false };
 	
 	componentDidMount() {
 		this.authService.registerListener(this);
@@ -42,18 +43,46 @@ class AppRouterBase extends React.PureComponent<undefined, { isLoggedIn: boolean
 							<Toolbar>
 								<Link variant="h5" component={MyLink} href="/home" color="inherit">Tweam</Link>
 								<div style={{ flexGrow: 1 }}/>
-								<Link component={MyLink} href="/about" color="inherit">About</Link>
+								<Link component={MyLink} href="/teams" color="inherit">Teams</Link>
+								<Link component={MyLink} href="/about" color="inherit">
+									<GroupIcon/> About
+								</Link>
 							</Toolbar>
 						</AppBar>
 
 						<Paper>
 							<Route path="/" exact component={Index} />
-							<Route path="/login" component={About} />
-							<Route path="/users/" component={Users} />
+							<Route path="/teams" render={this.renderTeamList} />
+							<Route path="/team/:twitterHandle" component={Users} />
 						</Paper>
 					</div>
 				</Router>
 			</AuthProvider>
 		);
+	}
+
+	private readonly renderTeamList = () => {
+		const teams: TeamInterface[] = [
+			{ id: 1, members: [
+				{
+					id: 0,
+					name: 'robeeert',
+					profilePicUrl: 'https://pbs.twimg.com/profile_images/1091708094214795265/lm8MLEb3_400x400.jpg',
+					tokens: [],
+					twitterHandle: 'r0b3333rt'
+				},
+				{
+					id: 1,
+					name: 'Manfred Bausch',
+					profilePicUrl: 'https://pbs.twimg.com/profile_images/761559147892596736/7vBhzxZX_400x400.jpg',
+					tokens: [],
+					twitterHandle: 'ensoniq23'
+				},
+			], name: 'team 1' },
+			{ id: 2, members: [], name: 'team 2' },
+			{ id: 3, members: [], name: 'team 3' },
+		];
+
+		return <TeamList teams={teams} />
 	}
 }
