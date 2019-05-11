8c14c5fc6a28891c264b3612cf8dc9f35fc8fda7
Lotti <charlotte.proeller@gmx.de>
2019-05-11 12:35:16 +0200

    inital spring project

---
+++ matchingserver/src/main/java/de/tweam/matchingserver/MatchingserverApplication.java
@@ -0,0 +1,13 @@
+package de.tweam.matchingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchingserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchingserverApplication.class, args);
+	}
+
+}
---
+++ matchingserver/src/test/java/de/tweam/matchingserver/MatchingserverApplicationTests.java
@@ -0,0 +1,16 @@
+package de.tweam.matchingserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MatchingserverApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
